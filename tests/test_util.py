489018428ded1b80244654eefa15f50a84064982
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2018-05-31 11:16:24 +0200

    Test normalizepdf

--- tests/test_util.py
+++ tests/test_util.py
@@ -9,10 +9,8 @@ class UtilTestSuite(unittest.TestCase):
     
     
     def test_equidistant_bin_edges(self):
-        """
-        Test the function cherenkovdeconvolution.util.equidistant_bin_edges.
-        """
-        # test on random argument values
+        """Test the function cherenkovdeconvolution.util.equidistant_bin_edges."""
+        # test on random arguments
         for i in range(10):
             with self.subTest(i=i):
                 minimum  = np.random.uniform(-1)      # in [-1, 1)
@@ -37,21 +35,40 @@ class UtilTestSuite(unittest.TestCase):
     
     @unittest.skip("Not yet implemented")
     def test_fit_R(self):
+        """Test the function cherenkovdeconvolution.util.fit_R."""
         pass # TODO
     
     
-    @unittest.skip("Not yet implemented")
     def test_normalizepdf(self):
-        pass # TODO
+        """Test the function cherenkovdeconvolution.util.normalizepdf."""
+        # test on random arguments
+        for i in range(10):
+            with self.subTest(i=i):
+                num_bins = np.random.randint(1, 1000)
+                arr      = np.random.uniform(size = num_bins)
+                narr     = util.normalizepdf(arr)
+                self.assertTrue(np.any(arr != narr)) # not performed in place
+                self.assertEqual(len(narr), num_bins)
+                self.assertAlmostEqual(sum(narr), 1) # total equality violated by rounding
+                util.normalizepdf(arr, copy = False)
+                self.assertTrue(np.all(arr == narr)) # in place version
+        
+        # test exceptions
+        intarr = np.random.randint(1000, size = 10) # integer array
+        self.assertAlmostEqual(sum(util.normalizepdf(intarr)), 1)
+        with self.assertRaises(ValueError):
+            util.normalizepdf(intarr, copy = False) # in place only allowed on floats
     
     
     @unittest.skip("Not yet implemented")
     def test_smooth_polynomial(self):
+        """Test the function cherenkovdeconvolution.util.smooth_polynomial."""
         pass # TODO
     
     
     @unittest.skip("Not yet implemented")
     def test_chi2s(self):
+        """Test the function cherenkovdeconvolution.util.chi2s."""
         pass # TODO
     
     
