626b5106dfb3a51251693f1f0918cacf69c09dd7
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2019-03-25 13:58:10 +0100

    Recoding of labels and results

--- cherenkovdeconvolution/__init__.py
+++ cherenkovdeconvolution/__init__.py
@@ -19,4 +19,32 @@
 # You should have received a copy of the GNU General Public License
 # along with CherenkovDeconvolution.py.  If not, see <http://www.gnu.org/licenses/>.
 # 
+import numpy as np
+
 from cherenkovdeconvolution.methods.dsea import deconvolve as dsea
+
+# recode indices to resemble a unit range (no missing labels in between)
+def _recode_indices(bins, *inds):
+    # recode the training set
+    inds_bins = np.unique(np.concatenate(tuple(inds)))
+    inds_dict = dict(zip(inds_bins, range(len(inds_bins))))
+    inds_rec  = [ [inds_dict[i] for i in ind] for ind in inds ]
+    
+    # set up reverse recoding applied in _recode_result
+    recode_dict = dict(zip(inds_dict.values(), inds_dict.keys())) # map from values to keys
+    recode_dict[-1] = np.max(bins) # the highest original bin (may not be in y_train)
+    
+    return tuple([recode_dict, *inds_rec]) # return all recoded indices
+
+# recode a deconvolution result by reverting the initial recoding of the data
+def _recode_result(M, recode_dict):
+    is_vector = len(M.shape) == 1 # else, we are recoding a probability matrix
+    if is_vector:
+        M = M.reshape(1, len(M)) # treat the vector M as a matrix
+    r = np.zeros(( M.shape[0], np.max(list(recode_dict.values()))+1 ))
+    for key, val in recode_dict.items():
+        if key != -1:
+            r[:, val] = M[:, key] # else, the key was just included to store the maximum value
+    if is_vector:
+        r = r.reshape(r.shape[1]) # treat the vector M like a vector again
+    return r
---
+++ tests/jl/test_jl_init.py
@@ -0,0 +1,37 @@
+import unittest, os
+import numpy as np
+import cherenkovdeconvolution as py_init
+
+# import CherenkovDeconvolution with the alias 'jl_init' from Julia
+from julia import CherenkovDeconvolution
+jl_init = CherenkovDeconvolution # hack to achieve a lowercase alias unsupported by pyjulia..
+
+@unittest.skipUnless(os.environ.get('TEST_JULIA')=='true', "Set TEST_JULIA=true to enable test")
+class JlInitTestSuite(unittest.TestCase):
+    """Check the equivalence of cherenkovdeconvolution.__init__ between Python and Julia."""
+    
+    def test_jl_recode(self):
+        """Test the function cherenkovdeconvolution._recode_indices and _recode_result."""
+        for i in range(10):
+            with self.subTest(i = i):
+                bins_total = range(np.random.randint(2, 100))
+                bins = np.unique(np.random.randint(len(bins_total), size=len(bins_total)-1))
+                num_samples = np.random.randint(1, 1000)
+                x1 = np.random.choice(bins, size = num_samples)
+                x2 = np.random.choice(bins, size = num_samples)
+                py_dict, py_x1, py_x2 = py_init._recode_indices(bins_total, x1, x2)
+                jl_dict, jl_x1, jl_x2 = jl_init._recode_indices(bins_total, x1, x2)
+                np.testing.assert_array_equal(py_x1, jl_x1 - 1) # julia indices start at 1
+                np.testing.assert_array_equal(py_x2, jl_x2 - 1)
+                
+                f1 = py_init.util.normalizepdf(np.bincount(x1, minlength=len(bins_total)))
+                f2 = py_init.util.normalizepdf(np.bincount(x2, minlength=len(bins_total)))
+                f1_r = py_init.util.normalizepdf(np.bincount(py_x1, minlength=len(bins)))
+                f2_r = py_init.util.normalizepdf(np.bincount(py_x2, minlength=len(bins)))
+                py_f1 = py_init._recode_result(f1_r, py_dict) # recode the result
+                py_f2 = py_init._recode_result(f2_r, py_dict)
+                np.testing.assert_array_equal(f1, py_f1)
+                np.testing.assert_array_equal(f2, py_f2)
+                
+if __name__ == '__main__':
+    unittest.main()
