9d44215a02e5fe1e3bdefbfaa77f568433d3ed55
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2019-03-26 18:05:40 +0100

    Implemented adaptive stepsize with a test that is failing on the Julia side..

--- cherenkovdeconvolution/stepsize.py
+++ cherenkovdeconvolution/stepsize.py
@@ -21,6 +21,9 @@
 # 
 import numpy as np
 from warnings import warn
+from scipy.optimize import minimize_scalar
+import cherenkovdeconvolution.util as util
+from cherenkovdeconvolution.methods.run import (_C_l, _maxl_l, _tikhonov_binning)
 
 
 def decay_mul(eta, start = 1.0):
@@ -74,3 +77,66 @@ def decay_exp(eta, start = 1.0):
         return start * eta**(k-1) # pk and f_prev are not used, here
     return alpha_fun
 
+
+def alpha_adaptive_run(x_data, x_train, y_train, tau = 0, bins_y = None, bins_x = None):
+    """Return a function object with the signature required by the alpha parameter in DSEA.
+    
+    This object adapts the DSEA step size to the current estimate by maximizing the likelihood
+    of the next estimate in the search direction of the current iteration.
+    
+    Parameters
+    ----------
+    x_data : array-like, shape (n_samples,), nonnegative ints
+        The observable quantity of the observed data set.
+    
+    x_train : array-like, shape (n_training_samples,), nonnegative ints
+        The observable quantity of the training set.
+    
+    y_train : array-like, shape (n_training_samples,), nonnegative ints
+        The labels belonging to x_train.
+    
+    tau : float, optional
+        The regularization strength used while maximizing the likelihood.
+    
+    bins_y : array-like, shape (I,), nonnegative ints, optional
+        The I indices of the target quantity values, i.e. the unique values of y.
+    
+    bins_x : array-like, shape (J,), nonnegative ints, optional
+        The J indices of the observed clusters, i.e. the unique values of x.
+    """
+    if bins_y is None:
+        bins_y = range(np.max(y_train))
+    if bins_x is None:
+        bins_x = range(np.max(np.concatenate((x_data, x_train))))
+    
+    # set up the discrete deconvolution problem
+    R = util.fit_R(y_train, x_train, bins_y = bins_y, bins_x = bins_x)
+    g = util.fit_pdf(x_data, bins_x, normalize = False) # absolute counts instead of pdf
+    
+    # set up a negative log likelihood function to be minimized
+    C = _tikhonov_binning(R.shape[1]) # regularization matrix (from methods.run)
+    maxl_l = _maxl_l(R, g)            # function of f (from methods.run)
+    maxl_C = _C_l(tau, C)             # regularization term (from methods.run)
+    def negloglike(f): # regularized objective function
+        return maxl_l(f) + maxl_C(f)
+    
+    # return a step size function
+    def alpha_adaptive_run(k, pk, f):
+        a_min, a_max = _alpha_range(pk, f)
+        return minimize_scalar(
+          lambda a : negloglike(f + a*pk),
+          bounds = (a_min, a_max),
+          method = 'Bounded'
+        ).x # only return the minimizer x (minimize_scalar is from scipy.optimize)
+    return alpha_adaptive_run
+
+# range of admissible alpha values
+def _alpha_range(pk, f):
+    # find alpha values for which the next estimate would be zero in one dimension
+    a_zero = - (f[pk!=0] / pk[pk!=0]) # ignore zeros in pk, for which alpha is arbitrary
+    
+    # for positive pk[i] (negative a_zero[i]), alpha has to be larger than a_zero[i]
+    # for negative pk[i] (positive a_zero[i]), alpha has to be smaller than a_zero[i]
+    a_min = np.max(np.concatenate((a_zero[a_zero<0], [0]))) # select a_min=0 if no pk[i]>0 exists
+    a_max = np.min(a_zero[a_zero>=0])
+    return a_min, a_max
--- tests/jl/test_jl_stepsize.py
+++ tests/jl/test_jl_stepsize.py
@@ -1,5 +1,7 @@
 import unittest, os
 import numpy as np
+import cherenkovdeconvolution.discretize as discretize
+import cherenkovdeconvolution.util as util
 import cherenkovdeconvolution.stepsize as py_stepsize
 
 # import CherenkovDeconvolution with the alias 'jl_stepsize' from Julia
@@ -45,4 +47,36 @@ class JlStepsizeTestSuite(unittest.TestCase):
                       py_alpha_fun(k, pk, f_prev),
                       jl_alpha_fun(k, pk, f_prev)
                     )
+
+    
+    def test_jl_alpha_adaptive_run(self):
+        """Test the function cherenkovdeconvolution.stepsize.alpha_adaptive_run."""
+        from sklearn.datasets import load_iris
+        iris = load_iris()
+        
+        # discretize the observed quantity into up to 6 clusters
+        y_iris = iris.target # already discrete
+        bins_y = np.sort(np.unique(y_iris))
+        x_iris = discretize.TreeDiscretizer(iris.data, y_iris, 6).discretize(iris.data)
+        
+        for i in range(10):
+            p_iris = np.random.permutation(len(iris.target))
+            x_data  = x_iris[p_iris[0:50]]
+            y_data  = y_iris[p_iris[0:50]]
+            x_train = x_iris[p_iris[50:150]]
+            y_train = y_iris[p_iris[50:150]]
+            
+            # find some random f_prev and f_next
+            f_true = util.fit_pdf(y_data, bins_y)
+            f_next = util.normalizepdf(2 * np.random.rand(len(f_true)) * f_true)
+            f_prev = util.normalizepdf(2 * np.random.rand(len(f_true)) * f_true)
+            pk = f_next - f_prev
+            
+            # optimize the step size
+            py_a = py_stepsize.alpha_adaptive_run(x_data, x_train, y_train, 0, bins_y)
+            jl_a = jl_stepsize.alpha_adaptive_run(x_data+1, x_train+1, y_train+1, 0, bins_y+1)
+            self.assertAlmostEqual(py_a, jl_a)
+
+if __name__ == '__main__':
+    unittest.main()
 
