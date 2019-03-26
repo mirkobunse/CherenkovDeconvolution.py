f36ea146853af1d1d54423de30cdeeb507f41b2d
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2019-03-26 14:50:55 +0100

    The test which made the latest fixes possible

--- tests/jl/test_jl_ibu.py
+++ tests/jl/test_jl_ibu.py
@@ -1,6 +1,7 @@
 import unittest, os
 import numpy as np
 import cherenkovdeconvolution.util as util
+import cherenkovdeconvolution.discretize as discretize
 import cherenkovdeconvolution.methods.ibu as py_ibu
 
 # import CherenkovDeconvolution with the alias 'jl_ibu' from Julia
@@ -23,20 +24,27 @@ class JlDseaTestSuite(unittest.TestCase):
                 jl_B = jl_ibu._ibu_reverse_transfer(R, f_0)
                 np.testing.assert_allclose(py_B, jl_B)
     
-    @unittest.skip('Not yet implemented')
     def test_jl_ibu(self):
         """Test complete deconvolution runs with IBU."""
         from sklearn.datasets import load_iris
         iris = load_iris()
+        
+        # discretize the observed quantity into up to 6 clusters
+        y_iris = iris.target # already discrete
+        x_iris = discretize.TreeDiscretizer(iris.data, y_iris, 6).discretize(iris.data)
+        
         for i in range(10):
             p_iris = np.random.permutation(len(iris.target))
-            X_data  = iris.data[p_iris[0:50], :]
-            X_train = iris.data[p_iris[50:150], :]
-            y_train = iris.target[p_iris[50:150]]
+            x_data  = x_iris[p_iris[0:50]]
+            x_train = x_iris[p_iris[50:150]]
+            y_train = y_iris[p_iris[50:150]]
+            py_f = py_ibu.deconvolve_evt(x_data, x_train, y_train, K=1) # single iteration
+            jl_f = jl_ibu.ibu(x_data, x_train, y_train+1, K=1)
             np.testing.assert_allclose(py_f, jl_f)
-            py_f = py_ibu.deconvolve_evt(x_data, x_train, y_train)
-            jl_f = jl_ibu.ibu(x_data, x_train, y_train+1)
-            np.testing.assert_allclose(py_f, jl_f)
+            
+            # py_f = py_ibu.deconvolve_evt(x_data, x_train, y_train, K=10) # 10 iterations
+            # jl_f = jl_ibu.ibu(x_data, x_train, y_train+1, K=10)
+            # np.testing.assert_allclose(py_f, jl_f)
 
 if __name__ == '__main__':
     unittest.main()
